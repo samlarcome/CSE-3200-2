a2ccd77b7657c808cd184ed92aeefb66a70a8076
Samuel Larcome <87437514+samlarcome@users.noreply.github.com>
2023-10-11 20:25:12 -0400

    In class 10/11

--- FragmentCrimeCH9/app/src/main/java/com/example/fragmentcrimech9/CrimeFragmentDetail.kt
+++ FragmentCrimeCH9/app/src/main/java/com/example/fragmentcrimech9/CrimeFragmentDetail.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import com.example.fragmentcrimech9.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
 
 class CrimeFragmentDetail : Fragment() {
+    private lateinit var binding : FragmentCrimeDetailBinding
+    private lateinit var crime : Crime
 
+    // EMPTY
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // we do not call setContentView() <--- would get put in onViewCreate
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title ="Bad stuff crime",
+            date = Date(),
+            isSolved = false
+        )
     }
 
+    // JUST INFLATE THE VIEW
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +34,37 @@ class CrimeFragmentDetail : Fragment() {
     ): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
         // LEFT OFF HERE
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    // MOST OF OUR LOGIC GOES
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _ , _ , _ ->
+                // take the crime object and copy all data from it
+                crime = crime.copy(title = text.toString() )
+
+                //crime.title = text.toString()
+            }
+
+//            crimeDate.text = crime.title
+//            crimeDate.setOnClickListener {_ : View ->
+//                //crimeDate.text = crime.title
+//                crimeDate.text = crime.isSolved.toString()
+//            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                crimeDate.isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener{ _ , isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
     }
 
 }
